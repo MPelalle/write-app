73bbd1910fc997218f84ca89ecfb508dc60a5a9e
MPelalle <reynatho@gmail.com>
2025-02-28 22:36:16 -0300

    1.0

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
-import connectDB from "./src/config/db.js";
+import path from "path";
 
+import connectDB from "./src/config/db.js";
 import authRoutes from "./src/routes/authRoutes.js";
 import noteRoutes from "./src/routes/noteRoutes.js";
-import path from "path";
+
 const app = express();
 dotenv.config();
 connectDB();
-
 
 
 
@@ -19,28 +19,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*", credentials: true }));
 
-
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 
-
-app.get('/ping', (req, res) => {
-    res.send('API funcionando');
+app.get("/ping", (req, res) => {
+    res.send("API funcionando");
 });
-
 
 app.get("/api/auth/register", (req, res) => {
     res.json({ message: "Registro funcionando!" });
 });
 
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
-}
 
 
 const PORT = process.env.PORT || 3000;
